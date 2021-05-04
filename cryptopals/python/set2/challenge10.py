d5ad11f61f9e8f6dacbbf0f06ac43fde446d7efa
Philser <pk30.05@hotmail.de>
2021-05-04 02:20:13 -0400

    Create rust project Move python project

--- cryptopals/python/set2/challenge10.py
+++ cryptopals/python/set2/challenge10.py
@@ -1,12 +1,13 @@
+from Crypto.Cipher import AES
 import importlib.util
-spec = importlib.util.spec_from_file_location("cipher_lib", "../lib/cipher_lib.py")
+spec = importlib.util.spec_from_file_location(
+    "cipher_lib", "../lib/cipher_lib.py")
 cipher_lib = importlib.util.module_from_spec(spec)
 spec.loader.exec_module(cipher_lib)
-from Crypto.Cipher import AES
 
 key = b"YELLOW SUBMARINE"
 suite = AES.new(key, AES.MODE_ECB)
 enc = suite.encrypt(cipher_lib.pad_pkcs7("YELLOW SUBMARINE", 16).encode())
 dec = suite.decrypt(enc)
 
-print (dec)
+print(dec)
