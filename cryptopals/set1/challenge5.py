1468e6a1df8b3158fed3af8ecd73e65f1f3bbd10
Philser <pk30.05@hotmail.de>
2021-05-04 11:04:51 -0400

    Fixes

--- cryptopals/set1/challenge5.py
+++ cryptopals/set1/challenge5.py
@@ -1,5 +1,6 @@
 import importlib.util
-spec = importlib.util.spec_from_file_location("cipher_lib", "../lib/cipher_lib.py")
+spec = importlib.util.spec_from_file_location(
+    "cipher_lib", "../lib/cipher_lib.py")
 cipher_lib = importlib.util.module_from_spec(spec)
 spec.loader.exec_module(cipher_lib)
 
@@ -10,9 +11,10 @@ expected_cipher = "0b3637272a2b2e63622c2e69692a23693a2a3c6324202d623d63343c2a\
 26226324272765272a282b2f20430a652e2c652a3124333a653e2b2027630c692b202831652\
 86326302e27282f"
 
-encrypted = cipher_lib.encrypt_repeating_key_xor(to_encrypt, "ICE")
+encrypted = cipher_lib.encrypt_repeating_key_xor(
+    to_encrypt.encode(), "ICE".encode())
 
-if encrypted == expected_cipher:
+if encrypted.hex() == expected_cipher:
     print("The algorithm works!")
 else:
     print("Uh oh! Got unexpected result: " + encrypted)
